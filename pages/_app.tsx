f8ac24a9182b21d2a83ee827aa19f8f5a433ce93
Evan Rolfe <accounts@evanrolfe.me>
2025-10-06 06:17:22 +0100

    docs bg

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,11 @@ import '../app/globals.css'
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <div className="dark relative min-h-screen overflow-hidden bg-gradient-to-br from-[#0a0a0a] via-[#183838] to-[#0a0a0a] text-[#a3a3a3]">
-            {/* Background blur overlay to match main page */}
-            <div className="relative z-20">
+            <div className="relative z-10">
                 <Component {...pageProps} />
             </div>
+            {/* Solid background overlay for docs content only - positioned below navbar */}
+            <div className="absolute top-16 left-0 right-0 bottom-0 bg-[#0a0a0a] z-5" />
         </div>
     )
 }
