3ab92f385fadb8175d87d184d462db81e7746904
Evan Rolfe <accounts@evanrolfe.me>
2025-10-08 10:05:59 +0100

    basepath

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  basePath: '/website',
+  assetPrefix: '/website',
+  trailingSlash: true,
   output: 'export',
   devIndicators: false
 }
